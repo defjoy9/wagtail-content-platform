53d71679db58b19f8f12056d6b91902d71c34613
defjoy <94872697+defjoy9@users.noreply.github.com>
2025-11-29 14:28:43 +0100

    home: added basic page model

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from wagtail.models import Page
-
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
 
 class HomePage(Page):
     pass
@@ -10,3 +11,20 @@ class AboutPage(Page):
 
     class Meta:
         verbose_name = "About"
+
+class StandardPage(Page):
+    """
+    A generic, reusable content page.
+    Suitable for all simple informational pages.
+    """
+    template = "home/standard_page.html"
+
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
+
+    class Meta:
+        verbose_name = "Standard Page"
+        verbose_name_plural = "Standard Pages"
