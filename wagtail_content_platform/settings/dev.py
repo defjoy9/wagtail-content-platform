eb13cbf53bd60ef4bd465c2468922e1f04b9e9a0
defjoy <94872697+defjoy9@users.noreply.github.com>
2025-12-01 10:23:28 +0100

    updated settings for production deployment

--- wagtail_content_platform/settings/dev.py
+++ wagtail_content_platform/settings/dev.py
@@ -1,18 +1,25 @@
+# core wagtail_content_platform dev.py
 from .base import *
+from dotenv import load_dotenv
+import os
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
 
+# PostgreSQL
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-
-try:
-    from .local import *
-except ImportError:
-    pass
--- wagtail_content_platform/settings/production.py
+++ wagtail_content_platform/settings/production.py
@@ -1,14 +1,46 @@
+# wagtail_content_platform/settings/production.py
 from .base import *
+from dotenv import load_dotenv
+import os
+
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 DEBUG = False
+ALLOWED_HOSTS = ['defjoy.site']
 
-# ManifestStaticFilesStorage is recommended in production, to prevent
-# outdated JavaScript / CSS assets being served from cache
-# (e.g. after a Wagtail upgrade).
-# See https://docs.djangoproject.com/en/5.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+# Static
 STORAGES["staticfiles"]["BACKEND"] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
-try:
-    from .local import *
-except ImportError:
-    pass
+# Database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
+
+# Logging
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {"format": "{levelname} {asctime} {module} {message}", "style": "{"},
+    },
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": os.path.join(BASE_DIR, "django.log"),
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "django": {"handlers": ["file"], "level": "INFO", "propagate": True},
+    },
+}
